ef95ae9c68060324e73c9c69480f4ae8df0a6e1e
Tadas Sutkaitis <tadasas@gmail.com>
2020-05-26 14:50:37 +0300

    broken upx

--- build_static_only_go.sh
+++ build_static_only_go.sh
@@ -10,7 +10,7 @@ docker run --rm \
   -v "$PWD":/go/src/${REPO_URL} -w /go/src/${REPO_URL} \
   -e GOOS=linux \
   -e GOARCH=amd64 \
-   docker.pkg.github.com/fitbeard/libvirt_exporter/libvirt_go:2.0 go build --ldflags '-extldflags "-static"' -o ${BINARY_NAME}
+   docker.pkg.github.com/fitbeard/libvirt_exporter/libvirt_go:3.0 go build --ldflags '-extldflags "-static"' -o ${BINARY_NAME}
 
 strip ${BINARY_NAME}
 upx ${BINARY_NAME}
